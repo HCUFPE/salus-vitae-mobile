77a2ba749260308a4b5282c068714eb913de6363
Gabr1el94 <bielsystem64@gmail.com>
2018-09-30 21:23:35 -0300

    Validação tela de login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -14,6 +14,11 @@ export class LoginPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, private toastCtrl: ToastController) {
   }
 
+<<<<<<< HEAD
+
+
+=======
+>>>>>>> a8d5c901499e6e4d1d3cf2e990ca203193c205d8
   login() {
     if (this.credentials.name.toLocaleLowerCase() === 'enfermeira' && this.credentials.password === '123') {
       this.navCtrl.setRoot('TabsPage');
@@ -21,14 +26,12 @@ export class LoginPage {
       if (this.toast !== undefined) {
         this.toast.dismiss();
       }
-      
       this.toast = this.toastCtrl.create({
         message: 'Usuário ou/e senha incorreto(s).',
         showCloseButton: true,
         closeButtonText: 'Fechar',
         dismissOnPageChange: true
       });
-
       this.toast.present();
     }
   }
